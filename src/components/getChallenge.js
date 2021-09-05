8038f8918b66c354f4e9bd5fd90c16b64238d029
carlos <carlos16qf@gmail.com>
2021-09-05 06:29:16 -0500

    commit two

---
+++ src/components/getChallenge.js
@@ -0,0 +1,6 @@
+// export const getChallenge = async () =>{
+//     const url = `https://moadw-challenge.herokuapp.com/api/`
+//     const data = await fetch(url).then((res) => res.json())
+//     return data
+     
+// }
